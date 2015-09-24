018dd2d42a9732ca48fc5637bfe5465e1091424f
AMBROSINI Loïc <loic@1001pharmacies.com>
2015-09-24 16:33:10 +0200

    fix configuration file

--- DependencyInjection/MeupKaliClientExtension.php
+++ DependencyInjection/MeupKaliClientExtension.php
@@ -10,8 +10,10 @@
 
 namespace Meup\Bundle\KaliClientBundle\DependencyInjection;
 
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
 
 class MeupKaliClientExtension extends Extension
 {
@@ -19,6 +21,9 @@ class MeupKaliClientExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 
     public function getAlias()
